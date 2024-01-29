a85813da6d20e4405eeef2b48a67bb31d1804d7b
dhckdwls <codetama51@gmail.com>
2024-01-29 11:02:33 +0900

    (혼자) 테스트데이터 생성,삭제,수정 기능구현

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -14,9 +14,52 @@ public class UsrArticleController {
 	int lastArticleId;
 	List<Article> articles;
 
-	public UsrArticleController() { 
-		lastArticleId = 0;
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article foundArticle = null;
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				foundArticle = article;
+				break;
+			}
+		}
+		if (foundArticle != null) {
+			articles.remove(foundArticle);
+			return String.format("%d 번 게시글이 삭제되었습니다", id);
+		}
+		return String.format("%d번 게시물은 없습니다", id);
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+		Article foundArticle = null;
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				foundArticle = article;
+				break;
+			}
+		}
+		
+		if (foundArticle != null) {
+			foundArticle.setTitle(title);
+			foundArticle.setBody(body);
+			foundArticle.toString();
+			return String.format(foundArticle + "\n%d번 게시물이 수정되었습니다", id);
+		}
+		return "그런게시물은 없습니다";
+
+	}
+
+	public UsrArticleController() {
+		lastArticleId = 10;
 		articles = new ArrayList<>();
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			articles.add(new Article(i, title, body));
+		}
 	}
 
 	@RequestMapping("/usr/article/doAdd")
