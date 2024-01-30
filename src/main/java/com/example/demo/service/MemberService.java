86da9930dbb7a93a4d3bad69ec8c97c679eff7c3
dhckdwls <codetama51@gmail.com>
2024-01-30 14:22:44 +0900

    로그인 아이디 중복체크

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -16,21 +16,25 @@ public class MemberService {
 	}
 
 	public int doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+
+		Member existsMember = getMemberByLoginId(loginId);
+
+		if (existsMember != null) {
+			return -1;
+		}
+
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
 		return memberRepository.getLastInsertId();
 
 	}
 
+	private Member getMemberByLoginId(String loginId) {
+		return memberRepository.getMemberByLoginId(loginId);
+	}
+
 	public Member getMember(int id) {
 		return memberRepository.getMember(id);
 	}
-
-	public Member getMemberId(String loginId) {
-		return memberRepository.getMemberId(loginId);
-		
-	}
-
-
 
 }
