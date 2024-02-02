8ce1e97691f0e9acb96f456c005b1ef46c930eb6
dhckdwls <codetama51@gmail.com>
2024-02-02 15:58:24 +0900

    로그인 폼 및 로그인 처리 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,9 +9,7 @@ import com.example.demo.service.MemberService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 import com.example.demo.vo.ResultData;
-import com.example.demo.vo.Rq;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -40,40 +37,46 @@ public class UsrMemberController {
 		return ResultData.from("S-1", Ut.f("로그아웃 되었습니다"));
 	}
 
-	@RequestMapping("/usr/member/doLogin")
-	public String doLogin(HttpServletRequest req,HttpSession httpSession,Model model, String loginId, String loginPw) {
-		
-//		Rq rq = (Rq) req.getAttribute("rq");
-//		
-//		if (rq.isLogined()) {
-////			return ResultData.from("F-A", "이미 로그인 상태입니다");
-//			return Ut.jsReplace(loginPw, loginId, loginPw);
-//		}
-//
-//		if (Ut.isNullOrEmpty(loginId)) {
-////			return ResultData.from("F-1", "아이디를 입력해주세요");
-//			return "아이디를 입력해주세요";
-//		}
-//		if (Ut.isNullOrEmpty(loginPw)) {
-////			return ResultData.from("F-2", "비밀번호를 입력해주세요");
-//			return "비밀번호를 입력해주세요";
-//		}
-//
-//		Member member = memberService.getMemberByLoginId(loginId);
-//
-//		if (member == null) {
-////			return ResultData.from("F-3", Ut.f("%s(은)는 존재하지 않는 아이디입니다", loginId));
-//			return "존재하지 않는 아이디입니다";
-//		}
-//
-//		if (member.getLoginPw().equals(loginPw) == false) {
-////			return ResultData.from("F-4", Ut.f("비밀번호가 일치하지 않습니다"));
-//			return "비밀번호가 일치하지 않습니다";
-//		}
-//
-//		httpSession.setAttribute("loginedMemberId", member.getId());
+	@RequestMapping("/usr/member/login")
+	public String showLogin(HttpSession httpSession) {
 
 		return "usr/member/login";
+	}
+
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
+
+		boolean isLogined = false;
+
+		if (httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+		}
+
+		if (isLogined) {
+			return Ut.jsHistoryBack("F-A", "이미 로그인 상태입니다");
+		}
+
+		if (Ut.isNullOrEmpty(loginId)) {
+			return Ut.jsHistoryBack("F-1", "아이디를 입력해주세요");
+		}
+		if (Ut.isNullOrEmpty(loginPw)) {
+			return Ut.jsHistoryBack("F-2", "비밀번호를 입력해주세요");
+		}
+
+		Member member = memberService.getMemberByLoginId(loginId);
+
+		if (member == null) {
+			return Ut.jsHistoryBack("F-3", Ut.f("%s(은)는 존재하지 않는 아이디입니다", loginId));
+		}
+
+		if (member.getLoginPw().equals(loginPw) == false) {
+			return Ut.jsHistoryBack("F-4", Ut.f("비밀번호가 일치하지 않습니다"));
+		}
+
+		httpSession.setAttribute("loginedMemberId", member.getId());
+
+		return Ut.jsReplace("S-1", Ut.f("%s님 환영합니다", member.getNickname()), "/");
 	}
 
 	@RequestMapping("/usr/member/doJoin")
--- src/main/webapp/WEB-INF/jsp/usr/member/login.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/member/login.jsp
@@ -2,58 +2,34 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <c:set var="pageTitle" value="LOGIN"></c:set>
 <%@ include file="../common/head.jspf"%>
-<br />
-<br />
-<div>
-	<a href="/">메인으로 돌아가기</a>
-</div>
 
-<script type="text/javascript">
-	var LoginForm__submitDone = false;
 
-	function LoginForm__submit(form) {
-		if (LoginForm__submitDone) {
-			alert('이미 처리중입니다');
-			return;
-		}
-		var loginId = form.loginId.value.trim();
-		var loginPw = form.loginPw.value.trim();
-
-		console.log('form.loginId.value : ' + loginId);
-		console.log('form.loginPw.value : ' + loginPw);
-
-		if (loginId.length == 0) {
-			alert('아이디를 입력해주세요');
-			form.loginId.focus();
-			return;
-		}
-		if (loginPw.length == 0) {
-			alert('비밀번호를 입력해주세요');
-			form.loginPw.focus();
-			return;
-		}
-
-		LoginForm__submitDone = true;
-		form.submit();
-
-	}
-</script>
-
-<form method="POST" action="doLogin" onsubmit="LoginForm__submit(this); return false;">
-	<div>
-		로그인 아이디 : <input autocomplete="off" type="text" placeholder="아이디를 입력해주세요" name="loginId" />
+<section class="mt-8 text-xl px-4">
+	<div class="mx-auto">
+		<form action="../member/doLogin" method="POST">
+			<table class="login-box table-box-1" border="1">
+				<tbody>
+					<tr>
+						<th>아이디</th>
+						<td><input autocomplete="off" type="text" placeholder="아이디를 입력해주세요" name="loginId" /></td>
+					</tr>
+					<tr>
+						<th>비밀번호</th>
+						<td><input autocomplete="off" type="text" placeholder="비밀번호를 입력해주세요" name="loginPw" /></td>
+					</tr>
+					<tr>
+						<th></th>
+						<td><input type="submit" value="로그인" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+		<div class="btns">
+			<button class="hover:underline" type="button" onclick="history.back();">뒤로가기</button>
+		</div>
 	</div>
-	<div>
-		로그인 비밀번호 : <input autocomplete="off" type="text" placeholder="비밀번호를 입력해주세요" name="loginPw" />
-	</div>
+</section>
 
-	<button type="submit">로그인</button>
-</form>
-
-
-<div>
-	<a style="color: green" href="../article/list">리스트로 돌아가기</a>
-</div>
 
 
 <%@ include file="../common/foot.jspf"%>
