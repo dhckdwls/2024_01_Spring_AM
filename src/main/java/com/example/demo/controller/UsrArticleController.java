da0ded8d5e660c112ecfbfb78ff98605d90b4a0d
dhckdwls <codetama51@gmail.com>
2024-02-05 11:33:05 +0900

    write 기능 정리(혼자),수정기능구현중

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -50,18 +50,26 @@ public class UsrArticleController {
 
 		return "usr/article/list";
 	}
+	
+	@RequestMapping("/usr/article/write")
+	public String showWrite(Model model) {
 
+		return "usr/article/write";
+	}
+	
 	@RequestMapping("/usr/article/doWrite")
 	@ResponseBody
-	public ResultData<Article> doWrite(HttpServletRequest req, String title, String body) {
+	public String doWrite(HttpServletRequest req, String title, String body) {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (Ut.isNullOrEmpty(title)) {
-			return ResultData.from("F-1", "제목을 입력해주세요");
+//			return ResultData.from("F-1", "제목을 입력해주세요");
+			return Ut.jsHistoryBack("F-1", "제목을 입력해주세요");
 		}
 		if (Ut.isNullOrEmpty(body)) {
-			return ResultData.from("F-2", "내용을 입력해주세요");
+//			return ResultData.from("F-2", "내용을 입력해주세요");
+			return Ut.jsHistoryBack("F-2", "내용을 입력해주세요");
 		}
 
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(rq.getLoginedMemberId(), title, body);
@@ -70,20 +78,28 @@ public class UsrArticleController {
 
 		Article article = articleService.getArticle(id);
 
-		return ResultData.newData(writeArticleRd, "article", article);
+		return Ut.jsReplace("S-1",Ut.f("%d번 게시물이 작성되었습니다.",id),"../article/detail?id=" +id);
+//		return ResultData.newData(writeArticleRd, "article", article);
 	}
 
+	@RequestMapping("/usr/article/modify")
+	public String showModify(Model model) {
+
+		return "usr/article/modify";
+	}
+	
 	// 로그인 체크 -> 유무 체크 -> 권한 체크 -> 수정
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData<Integer> doModify(HttpServletRequest req, int id, String title, String body) {
+	public String doModify(Model model,HttpServletRequest req, int id, String title, String body) {
 		Rq rq = (Rq) req.getAttribute("rq");
 
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), "id", id);
+			return Ut.jsHistoryBack("F-1",Ut.f("%d번 글은 존재하지 않습니다", id));
 		}
+		model.addAttribute("article", article);
 
 		ResultData loginedMemberCanModifyRd = articleService.userCanModify(rq.getLoginedMemberId(), article);
 
@@ -91,7 +107,7 @@ public class UsrArticleController {
 			articleService.modifyArticle(id, title, body);
 		}
 
-		return ResultData.from(loginedMemberCanModifyRd.getResultCode(), loginedMemberCanModifyRd.getMsg(), "id", id);
+		return Ut.jsReplace("S-1", Ut.f("%d번 글이 수정되었습니다", id), "../article/detail?id="+id);
 	}
 
 	// 로그인 체크 -> 유무 체크 -> 권한 체크 -> 삭제
--- src/main/webapp/WEB-INF/jsp/usr/article/write.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/article/write.jsp
@@ -1,29 +1,25 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<c:set var="pageTitle" value="ARTICLE MODIFY"></c:set>
+<c:set var="pageTitle" value="ARTICLE WRITE"></c:set>
 <%@ include file="../common/head.jspf"%>
 
 
 <section class="mt-8 text-xl px-4">
 	<div class="mx-auto">
-		<form action="../article/doModify" method="POST">
-			<table class="modify-box table-box-1" border="1">
+		<form action="../article/doWrite" method="POST">
+			<table class="write-box table-box-1" border="1">
 				<tbody>
 					<tr>
-						<th>번호 :</th>
-						<td><input autocomplete="off" type="text" placeholder="수정할 게시물번호" name="id" /></td>
-					</tr>
-					<tr>
 						<th>제목 :</th>
-						<td><input autocomplete="off" type="text" placeholder="수정할 제목을 입력하세요" name="title" /></td>
+						<td><input autocomplete="off" type="text" placeholder="제목을 입력하세요" name="title" /></td>
 					</tr>
 					<tr>
 						<th>내용 :</th>
-						<td><input autocomplete="off" type="text" placeholder="수정할 내용을 입력하세요" name="body" /></td>
+						<td><input autocomplete="off" type="text" placeholder="내용을 입력하세요" name="body" /></td>
 					</tr>
 					<tr>
 						<th></th>
-						<td><input type="submit" value="수정" /></td>
+						<td><input type="submit" value="작성" /></td>
 					</tr>
 				</tbody>
 			</table>
